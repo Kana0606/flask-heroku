0687a182a5177514a3d65ee1421a01296e21c0e5
Kana0606 <87840439+Kana0606@users.noreply.github.com>
2021-09-23 10:20:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,18 +26,25 @@ def hello():
     return "Hello Flask-Heroku"
 
 
-@app.route('/name')
-def name():
-        return"<font color=blue>คณนาฐ รัตนพิบูลย์</font> <br>เลขที่ 8 ม.4/10"
-
 @app.route('/api', methods=['GET'])
 def get_api():
     return jsonify(data)
 
+@app.route('/name')
+def name():
+        return"<font color=blue>คณนาฐ รัตนพิบูลย์</font> <br>เลขที่ 8 ม.4/10"
+
+@app.route('/video')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/hello/<string:name>')
 def Home(name):
 	return render_template('home.html', name_html=name)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
